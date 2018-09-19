f7ada84b01bb1a197a69cb701ffb5db724800e59
csadoff1 <43050255+csadoff1@users.noreply.github.com>
2018-09-19 19:29:13 -0400

    Fixed indentation

--- CtoF.java
+++ CtoF.java
@@ -1,25 +1,25 @@
 public class CtoF {
   
-public static double celsiusToFahrenheit (double n) {
-  return (n * (9.0/5.0) + 32.0);
-}
+  public static double celsiusToFahrenheit (double n) {
+    return (n * (9.0/5.0) + 32.0);
+  }
 
-public static double fahrenheitToCelsius (double n) {
-  return ((n - 32.0) * (5.0/9.0));
-}
+  public static double fahrenheitToCelsius (double n) {
+    return ((n - 32.0) * (5.0/9.0));
+  }
 
 // tests
-public static void main(String[] args) {
-System.out.println(celsiusToFahrenheit (0.0));
-System.out.println(celsiusToFahrenheit (100.0));
-System.out.println(celsiusToFahrenheit (-30.0));
-System.out.println(celsiusToFahrenheit (26.0));
-System.out.println(celsiusToFahrenheit (-1.0));
-System.out.println(fahrenheitToCelsius (0.0));
-System.out.println(fahrenheitToCelsius (100.0));
-System.out.println(fahrenheitToCelsius (-30.0));
-System.out.println(fahrenheitToCelsius (26.0));
-System.out.println(fahrenheitToCelsius (-1.0));
-}
+  public static void main(String[] args) {
+    System.out.println(celsiusToFahrenheit (0.0));
+    System.out.println(celsiusToFahrenheit (100.0));
+    System.out.println(celsiusToFahrenheit (-30.0));
+    System.out.println(celsiusToFahrenheit (26.0));
+    System.out.println(celsiusToFahrenheit (-1.0));
+    System.out.println(fahrenheitToCelsius (0.0));
+    System.out.println(fahrenheitToCelsius (100.0));
+    System.out.println(fahrenheitToCelsius (-30.0));
+    System.out.println(fahrenheitToCelsius (26.0));
+    System.out.println(fahrenheitToCelsius (-1.0));
+  }
 }
 
